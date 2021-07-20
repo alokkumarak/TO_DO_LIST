e7ec22600a17e1cd139ca128de84ccdb96b7c775
alokkumarak <68193957+alokkumarak@users.noreply.github.com>
2021-07-20 12:46:39 +0530

    Add files via upload
    added build folder

---
+++ build/precache-manifest.3e5430035b665aac4a458b9567ac423d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064c48fec759d592a1bd34449534f981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6cbc74433a706c62aa0",
+    "url": "/static/css/main.28ba6526.chunk.css"
+  },
+  {
+    "revision": "fe5fd5b0da23f6cf3969",
+    "url": "/static/js/2.3997d6f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3997d6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cbc74433a706c62aa0",
+    "url": "/static/js/main.9e82bcdd.chunk.js"
+  },
+  {
+    "revision": "affd7353ffc32ed2bae0",
+    "url": "/static/js/runtime-main.52b1acf7.js"
+  }
+]);
